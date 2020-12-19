ccda1fcb055df83c8e286f786faf8c510c12db10
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-12-19 11:30:11 +0530

    classnotes

---
+++ js/three.js
@@ -0,0 +1,13 @@
+var x = 40;
+var y = 60;
+
+var c = x + y;
+console.log("adding of ${x}+${y}: ", c);
+console.log(`adding of ${x}+${y}: `, c);
+//Template Literal
+//   ${}
+//   ${x}
+
+
+//console.log("string", variable);
+
