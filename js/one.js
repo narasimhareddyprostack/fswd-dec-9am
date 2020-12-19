976cea68df9e6017923b3c36724821ac42a7e6e5
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-12-19 11:19:09 +0530

    identifer

---
+++ js/one.js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 40;
+
+var c = x + y;
+console.log("Hello.....", c); // Javascript print
+function x() {}
+//hello
+
+/*
+  
+ 
+*/
---
+++ js/two.js
@@ -0,0 +1,23 @@
+var productName = "Iphone 12";
+
+console.log(productName);
+
+var price9 = "500USD";
+
+console.log(price);
+//JS Identifies Rule
+
+/*
+Identifier - should start with : a - z / A - Z , and : $, _ 
+             SubSequent Letter allowed - 0 to 9
+Should Not start with : Digits/special Char
+Should not use keywords
+
+--Variable, functions , class and Object
+Using Identifier - we will idetify the name of - variable, fun, class, object
+*/
+function order(){
+
+}
+
+var p99 = 10;
