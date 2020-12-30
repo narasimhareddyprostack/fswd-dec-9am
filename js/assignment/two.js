8db5ec1d16d65948fcb25d44cfb519cf7167b7eb
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-12-30 11:13:26 +0530

    events

---
+++ js/assignment/two.js
@@ -0,0 +1,13 @@
+let swathi = {
+  name: "swathi-fullstack",
+  email: "ssss@gmail.com",
+  status: function () {},
+};
+
+//access
+console.log(swathi.name);
+console.log(swathi.email);
+
+console.log(swathi.status());
+console.log(swathi.mobile);
+console.log(swathi.canceled());
---
+++ js/controlflow/switchone.js
@@ -0,0 +1,25 @@
+let day = "Iswar";
+switch (day) {
+  case "MONDAY":
+    console.log("Today is:", day);
+    break;
+  case "TUEDAY":
+    console.log("Today is:", day);
+    break;
+  case "WEDNESDAY":
+    console.log("Today is:", day);
+    break;
+  case "THURSDAY":
+    console.log("Today is:", day);
+    break;
+  case "FRIDAY":
+    console.log("Today is:", day);
+    break;
+  case "SATURDAY":
+    console.log("Today is:", day);
+    break;
+
+  default:
+    console.log("Hello, Please Select Day...");
+    break;
+}
